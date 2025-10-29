eefe22628928ef49795917a448b122be60d52571
Sümer <o773r@b260-pc004.dkfz-heidelberg.de>
2025-10-29 11:50:09 +0100

    Clear main branch — content moved to CellViT-main-05097e1

--- cell_segmentation/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# Utils
-#
-# @ Fabian Hörst, fabian.hoerst@uk-essen.de
-# Institute for Artifical Intelligence in Medicine,
-# University Medicine Essen
--- models/encoders/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-import logging
-
-logger = logging.getLogger(__name__)
-logger.addHandler(logging.NullHandler())
